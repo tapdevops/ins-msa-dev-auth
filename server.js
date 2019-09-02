09f1886d65ce39b817e0f1ef194af7c21b786db2
Ihsan Husaeri <ihusaeri@gmail.com>
2019-09-02 10:30:01 +0700

    add kafka request

--- server.js
+++ server.js
@@ -7,6 +7,7 @@
 	global.config = {};
 		  config.app = require( './config/app.js' );
 		  config.database = require( './config/database.js' )[config.app.env];
+	const ViewUserAuth = require( _directory_base + '/app/v1.0/Http/Models/ViewUserAuthModel.js' )
 
 /*
 |--------------------------------------------------------------------------
@@ -20,7 +21,7 @@
 
 	// Primary Variable
 	const App = Express();
-
+	
 	// Generate API Documentation
 	require( 'express-aglio' )( App,{
 		source: __dirname+ '/docs/source/index.md',
@@ -31,6 +32,55 @@
 			themeVariables: 'streak'
 		}
 	} );
+	var kafka = require("kafka-node"),
+	Producer = kafka.Producer,
+	Consumer = kafka.Consumer,
+	client = new kafka.KafkaClient({kafkaHost : "149.129.252.13:9092"}),
+	producer = new Producer(client),    
+	consumer = new Consumer(
+        client,
+        [
+            { topic: 'kafkaRequestData', partition: 0 },{ topic: 'kafkaDataCollectionProgress', partition: 0 },{ topic: 'kafkaResponse', partition: 0 }
+        ],
+        {
+            autoCommit: false
+        }
+    );
+	consumer.on('message', function (message) {
+		json_message = JSON.parse(message.value);
+		if(message.topic=="kafkaRequestData"){
+			//ada yang request data ke microservices
+			let reqDataObj;
+			let responseData = false;
+			console.log( json_message );
+			console.log( "msg_agg", json_message.agg );
+			if(json_message.msa_name=="auth"){
+				const matchJSON = JSON.parse( json_message.agg );
+				if( json_message.agg ){
+					console.log( "matchJSON", matchJSON );
+					const set = ViewUserAuth.aggregate( [	
+						// 	matchJSON
+					] );
+					reqDataObj = {
+						"msa_name":json_message.msa_name,
+						"model_name":json_message.model_name,
+						"requester":json_message.requester,
+						"request_id":json_message.request_id,
+						"data": set
+					}
+					responseData = true;	
+				}
+			}
+			if( responseData ){
+				let payloads = [
+					{ topic: "kafkaResponseData", messages: JSON.stringify( reqDataObj ), partition: 0 }
+				];
+				// producer.send( payloads, function( err, data ){
+				// 	console.log( "Send data to kafka", data );
+				// } );
+			}
+		}
+	});
 
 /*
 |--------------------------------------------------------------------------
