41e04fe160d8d50e85435daa5d990bd7c0e13494
ferdinand12345 <ferdshinodas@gmail.com>
2019-09-12 10:56:09 +0700

    Gani

--- config/app.js
+++ config/app.js
@@ -11,7 +11,7 @@
 		|--------------------------------------------------------------------------
 		*/
 			name: 'Microservice Auth',
-			env: 'prod', // prod, qa, dev,
+			env: 'qa', // prod, qa, dev,
 			port: {
 				dev: process.env.PORT || 4008,
 				qa: process.env.PORT || 5008,
